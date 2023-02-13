e594159be17928f3f963990f752f0a9acdc9e2ac
Sveta <skostyko@gmail.com>
2023-02-13 22:14:32 +0100

    add class OnlinerPage

---
+++ src/main/java/by/itacademy/svetakostyko/web/OnlinerPage.java
@@ -0,0 +1,6 @@
+package by.itacademy.svetakostyko.web;
+
+public class OnlinerPage {
+    public static final String URL = "https://www.onliner.by/";
+    public static final String COPYRIGHT = "//div[@class='footer-style__copy']";
+}
