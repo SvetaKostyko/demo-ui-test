80f5108db2b61b12e7d9b613ba4debca9dbf44bd
Sveta <skostyko@gmail.com>
2023-02-14 13:38:03 +0100

    refactor class OnlinerPage

--- src/main/java/by/itacademy/svetakostyko/web/OnlinerPage.java
+++ src/main/java/by/itacademy/svetakostyko/web/OnlinerPage.java
@@ -5,4 +5,6 @@ public class OnlinerPage {
     public static final String COPYRIGHT = "//div[@class='footer-style__copy']";
     public static final String BTN_ENTRANCE = "//div[@class='auth-bar__item auth-bar__item--text']";
 
+    public static final String LABEL_ENTRANCE = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+
 }
