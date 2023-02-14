3b2776b355b934f75ae0f8b93bb11935e2a72160
Sveta <skostyko@gmail.com>
2023-02-14 18:12:25 +0100

    refactor class OnlinerPage

--- src/main/java/by/itacademy/svetakostyko/web/OnlinerPage.java
+++ src/main/java/by/itacademy/svetakostyko/web/OnlinerPage.java
@@ -6,5 +6,6 @@ public class OnlinerPage {
     public static final String BTN_ENTRANCE = "//div[@class='auth-bar__item auth-bar__item--text']";
 
     public static final String LABEL_ENTRANCE = "//div[@class='auth-form__title auth-form__title_big auth-form__title_condensed-default']";
+    public static final String BTN_ENTRANCE_SECOND = "//*[@class='auth-button auth-button_primary auth-button_middle auth-form__button auth-form__button_width_full']";
 
 }
