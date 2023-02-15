24eed8757f15357b8879d0d559acf671e36e0814
Sveta <skostyko@gmail.com>
2023-02-15 14:21:55 +0100

    refactor AmazonPage

--- src/main/java/by/itacademy/svetakostyko/web/AmazonPage.java
+++ src/main/java/by/itacademy/svetakostyko/web/AmazonPage.java
@@ -1,11 +1,10 @@
 package by.itacademy.svetakostyko.web;
 
 public class AmazonPage {
-        public static final String URL = "https://www.amazon.com/";
-        public static final String COPYRIGHT = "//*[@id='navFooter']/div[5]/span";
-        public static final String CART = "//*[@id='nav-cart']";
-        public static final String LABEL_CARD_IS_EMPTY = "//div[@class='a-row sc-your-amazon-cart-is-empty']";
-        public static final String SIGH_IN_BTN = "//*[@id='a-autoid-0']/span";
-
-
-    }
+    public static final String URL = "https://www.amazon.com/";
+    public static final String COPYRIGHT = "//*[@id='navFooter']/div[5]/span";
+    public static final String CART = "//*[@id='nav-cart']";
+    public static final String LABEL_CARD_IS_EMPTY = "//div[@class='a-row sc-your-amazon-cart-is-empty']";
+    public static final String SIGH_IN_BTN = "//*[@id='a-autoid-0']/span";
+    public static final String SIGN_IN_LABEL = "(//h1[contains(@class, 'a-spacing-small')])";
+}
