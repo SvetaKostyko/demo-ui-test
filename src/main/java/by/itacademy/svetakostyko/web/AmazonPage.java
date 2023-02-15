4ea5bcc580de8727247c45af1b32e5e37c1b1539
Sveta <skostyko@gmail.com>
2023-02-15 08:09:16 +0100

    refactor AmazonPage

--- src/main/java/by/itacademy/svetakostyko/web/AmazonPage.java
+++ src/main/java/by/itacademy/svetakostyko/web/AmazonPage.java
@@ -1,4 +1,8 @@
 package by.itacademy.svetakostyko.web;
 
 public class AmazonPage {
-}
+        public static final String URL = "https://www.amazon.com/";
+        public static final String COPYRIGHT = "//*[@id='navFooter']/div[5]/span";
+        public static final String CART = "//*[@id='nav-cart']";
+
+    }
