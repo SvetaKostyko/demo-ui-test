2c62bec6cad61e96f223269b3d234edcfeba97f7
Sveta <skostyko@gmail.com>
2023-02-15 09:33:45 +0100

    refactor AmazonPage

--- src/main/java/by/itacademy/svetakostyko/web/AmazonPage.java
+++ src/main/java/by/itacademy/svetakostyko/web/AmazonPage.java
@@ -4,5 +4,6 @@ public class AmazonPage {
         public static final String URL = "https://www.amazon.com/";
         public static final String COPYRIGHT = "//*[@id='navFooter']/div[5]/span";
         public static final String CART = "//*[@id='nav-cart']";
+        public static final String LABEL_CARD_IS_EMPTY = "//div[@class='a-row sc-your-amazon-cart-is-empty']";
 
     }
