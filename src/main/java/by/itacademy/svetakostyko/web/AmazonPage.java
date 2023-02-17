8a0f4430f3defc682ed2e103d2287f40b785e96b
Sveta <skostyko@gmail.com>
2023-02-17 14:31:24 +0100

    refactor OnlinerPage

--- src/main/java/by/itacademy/svetakostyko/web/AmazonPage.java
+++ src/main/java/by/itacademy/svetakostyko/web/AmazonPage.java
@@ -5,8 +5,13 @@ public class AmazonPage {
     public static final String COPYRIGHT = "//*[@id='navFooter']/div[5]/span";
     public static final String CART = "//*[@id='nav-cart']";
     public static final String LABEL_CARD_IS_EMPTY = "//div[@class='a-row sc-your-amazon-cart-is-empty']";
-    public static final String SIGH_IN_BTN = "//*[@id='a-autoid-0']/span";
+    public static final String SIGH_IN_BTN_FROM_CARD = "//*[@id='a-autoid-0']/span";
     public static final String SIGN_IN_LABEL = "(//h1[contains(@class, 'a-spacing-small')])";
     public static final String CONTINUE_BTN = "//*[@id='continue']";
     public static final String ERROR_TEXT = "//*[@id='auth-email-missing-alert']/div/div";
+    public static final String EMAIL_INPUT = "//*[@id='ap_email']";
+    public static final String PASSWORD_INPUT = "//*[@id='ap_password']";
+    public static final String SIGN_IN_FROM_START = "//*[@id='nav-link-accountList']";
+    public static final String SIGN_IN_SECOND = "//*[@id='signInSubmit']";
+    public static final String CHECK_USER_IS_SIGNED = "//*[@id='nav-link-accountList-nav-line-1']";
 }
